da56ba0fe434e3716caf86770698ef563718b4f3
Judah Ben-Eliezer <judahbeneliezer@gmail.com>
2023-05-12 19:51:37 -0400

    made autosession auto save and restore

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -2,6 +2,8 @@ return {
     "rmagatti/auto-session",
     config = function()
         require("auto-session").setup({
+            auto_save_enabled = true,
+            auto_restore_enabled = true,
             session_lens = {
                 theme_conf = { border = true },
                 previewer = true,
