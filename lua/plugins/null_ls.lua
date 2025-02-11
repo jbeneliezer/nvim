b8eccf0c3b6dbd1241a5abe8fdb358be0ad24eef
jbeneliezer <Engineering@DENG1FDX.sfic.com>
2025-02-11 09:20:29 -0500

    added args to isort (null_ls.lua), changed ignore patterns (telescope.lua)

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,40 +1,40 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			debug = false,
-			sources = {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            debug = false,
+            sources = {
                 -- formatters
-				null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.black.with({
-					extra_args = { "--line-length", "120" },
-				}),
-				null_ls.builtins.formatting.clang_format.with({
-					extra_args = { "--style={BasedOnStyle: llvm, IndentWidth: 4, AccessModifierOffset: -4}" },
-				}),
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.isort.with({ extra_args = { "--line-length", "120", "--multi-line", "3" } }),
+                null_ls.builtins.formatting.black.with({
+                    extra_args = { "--line-length", "120" },
+                }),
+                null_ls.builtins.formatting.clang_format.with({
+                    extra_args = { "--style={BasedOnStyle: llvm, IndentWidth: 4, AccessModifierOffset: -4}" },
+                }),
                 -- diagnostics
-				null_ls.builtins.diagnostics.cppcheck.with({
-					extra_args = { "--enable=warning,performance,portability", "$FILENAME" },
-				}),
-				-- null_ls.builtins.diagnostics.ruff.with({
-				-- 	extra_args = { "--line-length", "120", "--ignore", "F403", "--ignore", "F405" },
-				-- }),
+                null_ls.builtins.diagnostics.cppcheck.with({
+                    extra_args = { "--enable=warning,performance,portability", "$FILENAME" },
+                }),
+                -- null_ls.builtins.diagnostics.ruff.with({
+                -- 	extra_args = { "--line-length", "120", "--ignore", "F403", "--ignore", "F405" },
+                -- }),
                 -- code actions
                 null_ls.builtins.code_actions.gitsigns,
-			},
-			-- on_attach = function(client, bufnr)
-				-- if client.supports_method("textDocument/formatting") then
-				-- vim.api.nvim_create_autocmd("BufWritePre", {
-				-- 	buffer = bufnr,
-				-- 	callback = function()
-				-- 		vim.lsp.buf.format({ bufnr = bufnr })
-				-- 	end,
-				-- })
-				-- end
-			-- end,
-		})
-	end,
+            },
+            -- on_attach = function(client, bufnr)
+            -- if client.supports_method("textDocument/formatting") then
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            -- 	buffer = bufnr,
+            -- 	callback = function()
+            -- 		vim.lsp.buf.format({ bufnr = bufnr })
+            -- 	end,
+            -- })
+            -- end
+            -- end,
+        })
+    end,
 }
